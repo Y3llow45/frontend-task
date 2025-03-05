c78c18a728d763fb3fff75637b390e02c16589c6
Борислав Христов <hristovborislav45@gmail.com>
2025-03-05 19:29:43 +0200

    Navigate to BannerCreate page by clicking FAB

--- src/components/FAB.tsx
+++ src/components/FAB.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@mui/joy'
 import Add from '@mui/icons-material/Add'
+import { Link } from 'react-router-dom'
 
 export default function FAB() {
     return (
-        <Button style={{
+        <Button 
+          component={Link}
+          to="/banners/create"
+          style={{
             position: 'fixed',
             right: '72px',
             bottom: '32px',
