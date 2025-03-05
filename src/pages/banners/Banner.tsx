b30b3dc6331e4e07d69c8c3b1be0f549cc0f21bb
Борислав Христов <hristovborislav45@gmail.com>
2025-03-05 21:24:56 +0200

    Reuse BannerForm in Banner page
    If banner hasn't loaded yet show "Loading..." text

--- src/pages/banners/Banner.tsx
+++ src/pages/banners/Banner.tsx
@@ -1,23 +1,35 @@
-import { Grid } from '@mui/joy'
-import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import { usePageData } from '../../context/page-data/page-data.context.ts'
+import BannerForm from '../../components/BannerForm.tsx'
+import BannerService from '../../services/banner.service.ts'
+import { BannerDto } from '../../services/dto/banner.dto.ts'
 
 export default function Banner() {
     const { setPageData } = usePageData()
+    const { id } = useParams<{ id: string }>()
+    const navigate = useNavigate()
+    const [banner, setBanner] = useState<BannerDto | null>(null)
 
-    useEffect(() => {
-        setPageData({ title: 'Banners > ' })
-    }, [setPageData])
-    const { id } = useParams()
+    useEffect(() => {        
+        if (id) {
+            setPageData({ title: `Banners > Edit ${id}` })
+            BannerService.getBanner(id).then((data) => {
+                if (data) {
+                    setBanner(data);
+                }
+            });
+        }
+    }, [setPageData, id])
 
-    return (
-        <Grid
-            container
-            spacing={2}
-        >
-            <h1>Todo implement</h1>
-            <h1>ID: {id}</h1>
-        </Grid>
-    )
+    const handleSubmit = async (data: BannerDto) => {
+        await BannerService.updateBanner(id!, data);
+        navigate('/banners');
+        };
+    
+        if (!banner || !id) {
+            return <div>Loading...</div>;
+        }
+    
+    return <BannerForm initialData={banner} onSubmit={handleSubmit} submitLabel="Save Changes" />;
 }
